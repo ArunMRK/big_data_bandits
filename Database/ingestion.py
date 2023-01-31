6aa23d0dd050e581a7a0a82a91efa8a19571dc07
Justas Bauras <justasb@hotmail.co.uk>
2023-01-31 16:11:00 +0000

    created function that extracts the user data and stores it in a dict

--- Database/ingestion.py
+++ Database/ingestion.py
@@ -39,9 +39,49 @@ print('TRYING DB CONNECTION\n')
 conn = get_db_connection()
 print('TRYING DB CONNECTION\n')
 
+import ast
 
-def extract_user_details(message):
-    return
+
+def split_name(name: str) -> list:
+    """Split fullname into a first and second name. Returns a list where the first element is the first name and the second element is the last name
+    """
+    if name:
+        return name.split(" ")
+    else:
+        return [None, None]
+
+
+def unix_to_date(timestamp: int) -> datetime.date:
+    """Take in unix timestamp (in ms, so have to divide by 1,000 to get seconds) and return date"""
+    timestamp /= 1000
+    time_and_date = datetime.datetime.fromtimestamp(
+        timestamp)
+    return time_and_date.date()
+
+
+def extract_user_details(message: str) -> dict:
+    """Extracts the user details and stores them in a dict"""
+    data = ast.literal_eval(message)["log"]
+    user_data = data.split("data = ")[1]
+    raw_data = ast.literal_eval(user_data)
+
+    name = split_name(raw_data["name"])
+    dob_date = unix_to_date(raw_data["date_of_birth"])
+    date_created = unix_to_date(raw_data["account_create_date"])
+    full_address = raw_data["address"].split(",")
+    postcode = full_address[-1]
+    address = ", ".join(full_address[:-1])
+
+
+    user_dict = {"user_id": raw_data["user_id"], "first": name[0],
+        "second": name[1], "address": address, "postcode": postcode, 
+        "dob_date": dob_date, "height": raw_data["height_cm"],
+        "weight": raw_data["weight_kg"], "gender": raw_data["gender"], 
+        "email": raw_data["email_address"], "date_created": date_created,
+        "original_source": raw_data["original_source"],
+        "bike_serial": raw_data["bike_serial"]}
+
+    return user_dict
 
 
 def upload_user_details_to_db(details):
@@ -158,20 +198,3 @@ while cont == True:
 
     except KeyboardInterrupt:
         c.close()
-
-
-def split_name(name: str) -> list:
-    """Split fullname into a first and second name. Returns a list where the first element is the first name and the second element is the last name
-    """
-    if name:
-        return name.split(" ")
-    else:
-        return [None, None]
-
-
-def unix_to_date(timestamp: int) -> datetime.date:
-    """Take in unix timestamp (in ms, so have to divide by 1,000 to get seconds) and return date"""
-    timestamp /= 1000
-    time_and_date = datetime.datetime.fromtimestamp(
-        timestamp)
-    return time_and_date.date()
